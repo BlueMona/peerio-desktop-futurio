3f74d2f0e7d7f52e6ddbbca5c11823f43bff1a8e
Anri Asaturov <anri82@gmail.com>
2017-11-10 20:19:37 +0200

    fix

--- scripts/publish_release.js
+++ scripts/publish_release.js
@@ -29,7 +29,7 @@ github.authenticate({
 });
 
 const version = JSON.parse(fs.readFileSync('../peerio-desktop/package.json', 'utf-8')).version;
-return github.repos.getReleaseByTag({ owner: 'PeerioTechnologies', repo: 'peerio-desktop-futurio', tag: version })
+return github.repos.getReleaseByTag({ owner: 'PeerioTechnologies', repo: 'peerio-desktop-futurio', tag: `v${version}` })
     .then(res => {
         const releaseId = res.data.id;
         if (!releaseId) throw new Error('Release not found.');
