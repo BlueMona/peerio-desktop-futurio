fbfd0f03ebd26871623af06844191869623a9e1c
Anri Asaturov <anri82@gmail.com>
2017-11-10 15:09:08 +0200

    fix ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 cd ..
 cd peerio-desktop
 ./scripts/deploy-futurio.sh
-#cd ..
-#cd peerio-desktop-futurio
-# VERSION="$(node ./scripts/get-package-version.js)"
-#./.circleci/ok.sh create_release PeerioTechnologies peerio-desktop-futurio v$VERSION
+cd ..
+cd peerio-desktop-futurio
+VERSION="$(node ./scripts/get-package-version.js)"
+./scripts/ok.sh create_release PeerioTechnologies peerio-desktop-futurio v$VERSION
