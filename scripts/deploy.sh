361af5f9eb1b828037cf71a5f54cccc3df393d36
Anri Asaturov <anri82@gmail.com>
2017-11-10 15:20:41 +0200

    fix ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,5 @@ cd peerio-desktop
 cd ..
 cd peerio-desktop-futurio
 VERSION="$(node ./scripts/get-package-version.js)"
+echo "GOT VERSION $VERSION"
 ./scripts/ok.sh create_release PeerioTechnologies peerio-desktop-futurio v$VERSION
