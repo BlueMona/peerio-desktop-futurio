57e4cfa71e75c9a6fd3e5647792e19f703425990
Anri Asaturov <anri82@gmail.com>
2017-11-12 03:25:34 +0200

    something that's supposed to prevent building when there's nothing new in desktop repo [skip ci]

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 cd ..
 cd peerio-desktop
+
+msg=$( git log --oneline -n 1 | grep "chore(release):" )
+
+if [ ! -z "$msg" ]; then
+    echo 'last commit is release. not building.'
+    exit 1
+fi
 ./scripts/deploy-futurio.sh
 
 cd ..
