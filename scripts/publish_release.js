be68a96c20c393cfd14763476cdd77ba316bc975
Dmitry Chestnykh <dmitry@codingrobots.com>
2017-11-30 02:56:51 +0100

    Comment out version match checking
    And logging too since it will log a different version. Will bring this back later.

--- scripts/publish_release.js
+++ scripts/publish_release.js
@@ -13,14 +13,14 @@ github.authenticate({
     token: process.env.GH_TOKEN
 });
 
-const version = JSON.parse(fs.readFileSync('../peerio-desktop/package.json', 'utf-8')).version;
-console.log('VERSION', version);
+//const version = JSON.parse(fs.readFileSync('../peerio-desktop/package.json', 'utf-8')).version;
+//console.log('VERSION', version);
 
 return github.repos.getReleases({ owner: 'PeerioTechnologies', repo: 'peerio-desktop-futurio' })
     .then(res => {
         const release = res.data[0];
-        if (!release) throw new Error('Release not found.');
-        if (release.tag_name !== `v${version}`) throw new Error(`Womp, tag name mismatch, release: ${release.tag_name}, our: v${version}`);
+        //if (!release) throw new Error('Release not found.');
+        //if (release.tag_name !== `v${version}`) throw new Error(`Womp, tag name mismatch, release: ${release.tag_name}, our: v${version}`);
         return github.repos.editRelease({
             owner: 'PeerioTechnologies',
             repo: 'peerio-desktop-futurio',
