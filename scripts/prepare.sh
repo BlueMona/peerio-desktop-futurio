599a4d0676d8f29a32256829d22ea8209642345b
Dmitry Chestnykh <dmitry@codingrobots.com>
2018-03-20 13:39:10 +0100

    Fix prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 cd ..
-git clone https://github.com/PeerioTechnologies/peerio-desktop.git#newfs
+git clone https://github.com/PeerioTechnologies/peerio-desktop.git
 cd peerio-desktop
+git checkout newfs
 ./scripts/install-win-build-tools.sh
 brew link graphicsmagick
 
