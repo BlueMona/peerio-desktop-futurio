e88a8e096cedf89bc50ffe616b700b874e2eed10
Dmitry Chestnykh <dmitry@codingrobots.com>
2018-09-10 14:32:06 +0200

    chore: insert pwd's to debug CircleCI failures

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+pwd
 cd ..
 cd peerio-desktop
 
+pwd
 ./scripts/deploy-futurio.sh
+pwd
 
 cd ..
 cd peerio-desktop-futurio
 npm i @octokit/rest bluebird
+pwd
 node ./scripts/publish_release.js
